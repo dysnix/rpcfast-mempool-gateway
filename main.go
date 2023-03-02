208d8a9cc2983452312bf7fb7a55d8a6f4a0fade
Daniel Yavorovych <daniel@dysnix.com>
2023-03-02 16:48:53 +0200

    add /api/peers endpoint

--- main.go
+++ main.go
@@ -5,7 +5,10 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"flag"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"net/http"
 )
@@ -25,11 +28,34 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func servePeers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-master:6379",
+		Password: "", // no password set
+		DB:       2,  // use default DB
+		PoolSize: 100,
+	})
+
+	data := rdb.ZRangeWithScores(ctx, "peers", 0, 10)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(data)
+}
+
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/api/peers", servePeers)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
